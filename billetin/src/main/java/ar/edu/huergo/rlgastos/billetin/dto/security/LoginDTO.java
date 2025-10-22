ef7b8a259314bab3c1e7dabc00a403dd598ae413
LJaneiro <ljaneiro@alumno.huergo.edu.ar>
2025-10-21 22:12:51 -0300

    no se puede iniciar sesion, muy buen tp

--- billetin/src/main/java/ar/edu/huergo/rlgastos/billetin/dto/security/LoginDTO.java
+++ billetin/src/main/java/ar/edu/huergo/rlgastos/billetin/dto/security/LoginDTO.java
@@ -9,6 +9,9 @@ public record LoginDTO(
         @Email(message = "El nombre de usuario debe ser un email válido") 
         String username,
         @NotBlank(message = "La contraseña es requerida") 
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{16,}$", message = "La contraseña debe tener al menos 16 caracteres, una mayúscula, una minúscula, un número y un carácter especial")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{16,}$",
+                message = "La contraseña debe tener al menos 16 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+        )
         String password) {
 }
